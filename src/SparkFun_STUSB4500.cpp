cd3e645fbfd23495b7dcc24b590e0a65d739231a
awende <alex.wende@sparkfun.com>
2021-03-05 14:00:26 -0700

    v1.1.3
    Fixed issue when writing default values

--- src/SparkFun_STUSB4500.cpp
+++ src/SparkFun_STUSB4500.cpp
@@ -86,6 +86,40 @@ void STUSB4500::read(void)
   }
   
   CUST_ExitTestMode();
+
+  // NVM settings get loaded into the volatile registers after a hard reset or power cycle.
+  // Below we will copy over some of the saved NVM settings to the I2C registers
+  uint8_t currentValue;
+
+  //PDO Number
+  setPdoNumber((sector[3][2] & 0x06)>>1);
+
+
+  //PDO1 - fixed at 5V and is unable to change
+  //setVoltage(1,5.0);
+
+  currentValue = (sector[3][2]&0xF0) >> 4;
+  if(currentValue == 0)      setCurrent(1,0);
+  else if(currentValue < 11) setCurrent(1,currentValue * 0.25 + 0.25);
+  else                       setCurrent(1,currentValue * 0.50 - 2.50);
+
+  //PDO2
+  setVoltage(2,sector[4][1]*0.2);
+
+  currentValue = (sector[3][4]&0x0F);
+  if(currentValue == 0)      setCurrent(2,0);
+  else if(currentValue < 11) setCurrent(2,currentValue * 0.25 + 0.25);
+  else                       setCurrent(2,currentValue * 0.50 - 2.50);
+
+  //PDO3
+  setVoltage(3,(((sector[4][3]&0x03)<<8) + sector[4][2])*0.05);
+
+  currentValue = (sector[3][5]&0xF0) >> 4;
+  if(currentValue == 0)      setCurrent(3,0);
+  else if(currentValue < 11) setCurrent(3,currentValue * 0.25 + 0.25);
+  else                       setCurrent(3,currentValue * 0.50 - 2.50);
+
+
 }
 
 void STUSB4500::write(uint8_t defaultVals)
@@ -266,6 +300,8 @@ void STUSB4500::setCurrent(uint8_t pdo_numb, float current)
     -current from 3.0-5.0A is set in 0.50A steps
   */
   uint8_t nvmCurrent;
+
+  if(current > 5.0) current = 5.0;
 
   if(current < 0.5)     nvmCurrent = 0;
   else if(current <= 3) nvmCurrent = (4*current)-1;
